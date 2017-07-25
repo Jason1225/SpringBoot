1a0b588728f2c07b26831bb4f3747b53fc63197d
gaoxu <gaoxu@benlai.com>
2017-07-25 15:04:30 +0800

    提交

---
+++ hello/src/com/TestJar.java
@@ -0,0 +1,10 @@
+package com;
+public class TestJar {
+	public static void main(String[] args) {
+		String port=args[0];
+		String context=args[1];
+		TestJar obj = new TestJar();
+		System.out.println("this is simulator:"+port);
+		System.out.println("this is simulator:"+context);
+	}
+}
